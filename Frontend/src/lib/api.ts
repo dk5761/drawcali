d4235e9d61ea999c65a9a9ffd4af83851a16b153
dk-getrezolv <darshan.kadam@getrezolv.com>
2025-07-12 11:54:10 +0530

    updated the env

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/v1";
+const API_BASE_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
 // Create axios instance
 const api = axios.create({
